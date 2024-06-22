fa5a6930378c7c801a769fed4d9f41d50270c69e
ab987 <ab9878987@gmail.com>
2024-06-21 21:10:13 -0500

    Initial commit

---
+++ src/main/java/dev/ab/lpcolorapi/LPColorAPI.java
@@ -0,0 +1,17 @@
+package dev.ab.lpcolorapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LPColorAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
